bc24d8995bcf7c960fbc75ce9085fd727a41c762
SimoRinaldi <s.rinaldi.tech@gmail.com>
2026-08-04 15:11:08 +0200

    Entità rubric-set

--- libs/server/rubrics/src/lib/rubrics/entities/rubric-set.entity.ts
+++ libs/server/rubrics/src/lib/rubrics/entities/rubric-set.entity.ts
@@ -1,1 +1,17 @@
-export class RubricSet {}
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { RubricLevel } from './rubric-level.entity'; 
+
+@Entity('rubric_set')
+export class RubricSet {
+    @PrimaryGeneratedColumn()                                                                            
+    id: number;                                                                                          
+                                                                                                                                                                                 
+    @Column({type: 'boolean', default: false})                                                         
+    yes_no: boolean; 
+
+    // Un RubricSet ha MOLTI RubricLevel                                                   
+    @OneToMany(() => RubricLevel, (level) => level.rubricSet, {                                          
+        cascade: true,                      
+    })                                                                                                   
+    levels: RubricLevel[]; 
+}
