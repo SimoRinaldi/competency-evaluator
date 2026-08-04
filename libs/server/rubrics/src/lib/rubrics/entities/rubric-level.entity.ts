f0cf49d6336ccb694569a09cd88c0aff6cfc3d45
SimoRinaldi <s.rinaldi.tech@gmail.com>
2026-08-04 15:21:27 +0200

    Entità rubric-level

--- libs/server/rubrics/src/lib/rubrics/entities/rubric-level.entity.ts
+++ libs/server/rubrics/src/lib/rubrics/entities/rubric-level.entity.ts
@@ -1,1 +1,25 @@
-export class RubricLevel {}
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';               
+import { RubricSet } from './rubric-set.entity';       
+
+@Entity('rubric_level')
+export class RubricLevel {
+    @PrimaryGeneratedColumn()                                                                            
+    id: number;                                                                                         
+                                                                                                           
+    @Column({type: 'varchar', length: 255})                                                            
+    description: string;                                                                                
+                                                                                                                                         
+    @Column({type: 'int'})                                                                             
+    rank: number;                       
+
+    // Molti RubricLevel appartengono a UN SOLO RubricSet                                                
+    @ManyToOne(() => RubricSet, (rubricSet) => rubricSet.levels, {                                       
+        onDelete: 'CASCADE',          
+    })  
+
+    @JoinColumn({name: 'rubric_set_id'})   
+    rubricSet: RubricSet;                                                                               
+                                                                                                                                
+    @Column()                                                                                            
+    rubric_set_id: number;
+}
