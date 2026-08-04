2b6551ee89a7cce9499354f1976da1ff1e760664
SimoRinaldi <s.rinaldi.tech@gmail.com>
2026-08-04 17:12:12 +0200

    Creazione DTO per le due entità Rubrics

--- libs/server/rubrics/src/lib/rubrics/dto/create-rubric.dto.ts
+++ libs/server/rubrics/src/lib/rubrics/dto/create-rubric.dto.ts
@@ -1,1 +1,21 @@
-export class CreateRubricDto {}
+import { IsBoolean, IsString, IsInt, IsArray, ValidateNested, Min } from 'class-validator';                           
+import { Type } from 'class-transformer'; 
+
+export class CreateRubricLevelDto {
+    @IsString()
+    description: string;
+
+    @IsInt()
+    @Min(1) 
+    rank: number;
+}
+
+export class CreateRubricSetDto {
+    @IsBoolean()
+    yes_no: boolean;
+
+    @IsArray()
+    @ValidateNested({each: true})
+    @Type(() => CreateRubricLevelDto)
+    levels: CreateRubricLevelDto[];
+}
