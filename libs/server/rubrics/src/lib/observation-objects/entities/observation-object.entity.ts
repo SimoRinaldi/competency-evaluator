e097523a5ffcb824fe996f363ba2bca83508c14f
SimoRinaldi <s.rinaldi.tech@gmail.com>
2026-08-04 15:56:41 +0200

    Entità observation-object

--- libs/server/rubrics/src/lib/observation-objects/entities/observation-object.entity.ts
+++ libs/server/rubrics/src/lib/observation-objects/entities/observation-object.entity.ts
@@ -1,1 +1,10 @@
-export class ObservationObject {}
+import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';                                                        
+                                                                                                           
+@Entity('observation_object')                                                                          
+export class ObservationObject {                                                                       
+    @PrimaryGeneratedColumn()                                                                            
+    id: number;                                                                                         
+                                                                                                                                                  
+    @Column({type: 'text', nullable: true})                                                            
+    description: string;                                      
+}                                           
