1078a393f702be7bb5c9b908eb96f35e383b6582
gmatteo <git+github@bit-relay.com>
2026-08-04 11:28:35 +0200

    feat(competencies): implementazione entità TypeORM per Competency e SubCompetency
    - Definite le colonne e i vincoli di tabella per competencies e sub_competencies
    - Configurata la relazione OneToMany/ManyToOne tra Competency e SubCompetency

--- libs/server/competencies/src/lib/competency/entities/competency.entity.ts
+++ libs/server/competencies/src/lib/competency/entities/competency.entity.ts
@@ -1,1 +1,29 @@
-export class Competency {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  Relation,
+} from 'typeorm';
+import { SubCompetency } from '../../sub-competency/entities/sub-competency.entity';
+
+@Entity('competencies')
+export class Competency {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    unique: true,
+    default: 'Unknown',
+  })
+  title!: string;
+
+  @Column({ type: 'int', nullable: false })
+  weight!: number;
+
+  @OneToMany(() => SubCompetency, (sub_competency) => sub_competency.id)
+  sub_competencies?: Relation<SubCompetency>[];
+}
--- libs/server/competencies/src/lib/sub-competency/entities/sub-competency.entity.ts
+++ libs/server/competencies/src/lib/sub-competency/entities/sub-competency.entity.ts
@@ -1,1 +1,57 @@
-export class SubCompetency {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import type { Relation } from 'typeorm';
+import { Competency } from '../../competency/entities/competency.entity';
+
+@Entity('sub_competencies')
+export class SubCompetency {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    unique: true,
+    default: 'Unknown',
+  })
+  title!: string;
+
+  @Column({ type: 'int', nullable: false })
+  weight!: number;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: true,
+  })
+  input?: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: true,
+  })
+  output?: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: true,
+  })
+  action?: string;
+
+  @Column({ type: 'int', nullable: false })
+  competency_id!: number;
+
+  @ManyToOne(() => Competency, (competency) => competency.id, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'competency_id' })
+  competency?: Relation<Competency>;
+}
