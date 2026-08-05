8685cca8aad4d57b51d75faba06f3e05283e26e0
SimoRinaldi <s.rinaldi.tech@gmail.com>
2026-08-06 00:08:30 +0200

    Creazione rubrics.controller.ts

--- libs/server/rubrics/src/lib/rubrics.controller.ts
+++ libs/server/rubrics/src/lib/rubrics.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, ParseIntPipe } from '@nestjs/common';
 import { ServerRubricsService } from './rubrics.service';
+import { CreateRubricSetDto } from './dto/create-rubric.dto'
 
 @Controller('rubrics')
 export class ServerRubricsController {
-  constructor(private serverRubricsService: ServerRubricsService) {}
-}
+
+  constructor(private readonly serverRubricsService: ServerRubricsService) {}
+
+  @Post()
+  create(@Body() createRubricSetDto: CreateRubricSetDto) {
+    return this.serverRubricsService.create(createRubricSetDto);
+  }
+
+  @Get()                                                                                                                                                                     
+  findAll() {                                                                                                                                                                
+    return this.serverRubricsService.findAll();                                                                                                                                    
+  }                                                                                                                                                                          
+                                                                                                                                                                                                                                                                                     
+  @Get(':id')                                                                                                                                                                
+  findOne(@Param('id', ParseIntPipe) id: number) {                                                                                                                                                                                                                                                             
+    return this.serverRubricsService.findOne(id);                                                                                                                                  
+  }                                                                                                                                                                          
+}  
