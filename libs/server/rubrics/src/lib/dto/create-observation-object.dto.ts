7226cb0aaff2724365d6c9a57e57bd7a16473614
SimoRinaldi <s.rinaldi.tech@gmail.com>
2026-08-07 00:02:59 +0200

    Aggiunto i sample alle api della lib rubrics

--- libs/server/rubrics/src/lib/dto/create-observation-object.dto.ts
+++ libs/server/rubrics/src/lib/dto/create-observation-object.dto.ts
@@ -1,7 +1,9 @@
-import { IsOptional, IsString } from 'class-validator';                                                        
+import { IsOptional, IsString } from 'class-validator';     
+import { ApiProperty } from '@nestjs/swagger';                                                   
                                                                                                                           
-export class CreateObservationObjectDto {                                                                                                                                                                                                                                                                    
+export class CreateObservationObjectDto {      
+    @ApiProperty({ example: 'Documento di sintesi PDF', required: false })
     @IsOptional()                                                                                                       
     @IsString()                                                                                                         
-    description?: string;                                                                                                                                                                                                                                                                                                                                                                                                     
+    description: string;                                                                                                                                                                                                                                                                                                                                                                                                     
 }
--- libs/server/rubrics/src/lib/dto/create-rubric.dto.ts
+++ libs/server/rubrics/src/lib/dto/create-rubric.dto.ts
@@ -1,19 +1,24 @@
 import { IsBoolean, IsString, IsInt, IsArray, ValidateNested, Min } from 'class-validator';                           
 import { Type } from 'class-transformer'; 
+import { ApiProperty } from '@nestjs/swagger';                                                                                                           
 
 export class CreateRubricLevelDto {
+    @ApiProperty({ example: 'Inadeguato', description: 'Testo che descrive questo livello' })
     @IsString()
     description: string;
 
+    @ApiProperty({ example: 1, description: 'Valore numerico (rango) del livello' })                                                                       
     @IsInt()
     @Min(1) 
     rank: number;
 }
 
 export class CreateRubricSetDto {
+    @ApiProperty({ example: false, description: 'True per valutazione binaria (Si/No), False per scala standard' })                                        
     @IsBoolean()
     yes_no: boolean;
 
+    @ApiProperty({ type: () => [CreateRubricLevelDto], description: 'Elenco dei livelli da creare insieme al set' })                                       
     @IsArray()
     @ValidateNested({each: true})
     @Type(() => CreateRubricLevelDto)
