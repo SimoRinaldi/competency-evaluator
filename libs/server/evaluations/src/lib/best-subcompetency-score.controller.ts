8dccc5975469881312eaa3c22425b5e51bfe69cd
SimoRinaldi <s.rinaldi.tech@gmail.com>
2026-08-13 17:20:18 +0200

    Aggiornato files di Best SubCompetency Score
    - controller (guardie)
    - service
    - repository

--- libs/server/evaluations/src/lib/best-subcompetency-score.controller.ts
+++ libs/server/evaluations/src/lib/best-subcompetency-score.controller.ts
@@ -1,38 +1,55 @@
 import { Controller, Get, Post, Body, Param, ParseIntPipe, Patch, Delete, UseGuards, ValidationPipe } from '@nestjs/common';
 import { ServerBestSubCompetencyScoresService } from './best-subcompetency-score.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard, Roles, RolesGuard } from '@server/security';
 import { CreateBestSubCompetencyScoreDto } from './dto/create-best-subcompetency-score.dto';
 import { UpdateBestSubCompetencyScoreDto } from './dto/update-best-subcompetency-score.dto';
+import { UserRole } from '@server/users';
 
 @ApiTags('BestSubCompetencyScores API')
 @Controller('best_subcompetency_scores')
 export class ServerBestSubCompetencyScoreController {
   constructor( private readonly serverBestSubCompetencyScoresService: ServerBestSubCompetencyScoresService ) {}
 
-  // guardie, roles e decoratori di swagger
+  // decoratori di swagger
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @ApiBearerAuth()
   create(@Body(ValidationPipe) createBestSubCompetencyScore: CreateBestSubCompetencyScoreDto) {
     return this.serverBestSubCompetencyScoresService.create(createBestSubCompetencyScore);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.TEST_DESIGNER)
+  @ApiBearerAuth()
   findAll() {
     return this.serverBestSubCompetencyScoresService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.TEST_DESIGNER)
+  @ApiBearerAuth()
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.serverBestSubCompetencyScoresService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @ApiBearerAuth()
   update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateBestSubCompetencyScoreDto: UpdateBestSubCompetencyScoreDto) {
     return this.serverBestSubCompetencyScoresService.update(id, updateBestSubCompetencyScoreDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  @ApiBearerAuth()
   remove(@Param('id', ParseIntPipe) id: number) {
-    return this.serverBestSubCompetencyScoresService.delete(id);
+    return this.serverBestSubCompetencyScoresService.remove(id);
   }
 }
