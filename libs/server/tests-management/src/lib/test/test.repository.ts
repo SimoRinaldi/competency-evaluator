a30f4ca26b15a576eaee932a7d91778fc7c0b818
SimoRinaldi <s.rinaldi.tech@gmail.com>
2026-08-30 11:15:31 +0200

    feat(ui,api): complete user evaluation page with interactive rubric

--- libs/server/tests-management/src/lib/test/test.repository.ts
+++ libs/server/tests-management/src/lib/test/test.repository.ts
@@ -28,14 +28,14 @@ export class TestRepository {
   async findAll(): Promise<TestEntity[]> {
     return this.repository.find({
       order: { id: 'ASC' },
-      relations: ['test_designer', 'subcompetencies'],
+      relations: ['test_designer', 'subcompetencies', 'subcompetencies.observation_object', 'subcompetencies.observation_object.indicators', 'subcompetencies.observation_object.indicators.rubric_set', 'subcompetencies.observation_object.indicators.rubric_set.levels'],
     });
   }
 
   async findById(id: number): Promise<TestEntity | null> {
     return this.repository.findOne({
       where: { id },
-      relations: ['test_designer', 'subcompetencies'],
+      relations: ['test_designer', 'subcompetencies', 'subcompetencies.observation_object', 'subcompetencies.observation_object.indicators', 'subcompetencies.observation_object.indicators.rubric_set', 'subcompetencies.observation_object.indicators.rubric_set.levels'],
     });
   }
 
@@ -43,7 +43,7 @@ export class TestRepository {
     return this.repository.find({
       where: { test_designer_id },
       order: { id: 'ASC' },
-      relations: ['test_designer', 'subcompetencies'],
+      relations: ['test_designer', 'subcompetencies', 'subcompetencies.observation_object', 'subcompetencies.observation_object.indicators', 'subcompetencies.observation_object.indicators.rubric_set', 'subcompetencies.observation_object.indicators.rubric_set.levels'],
     });
   }
 
